9555be2263e7d07cbb690189645c5e533ae773de
“Kalon22” <kalonacharjee@gmail.com>
2025-02-21 12:50:49 +0530

    aggregation not supporting array

--- feed/src/main/java/com/askthem/feed/controller/FeedController.java
+++ feed/src/main/java/com/askthem/feed/controller/FeedController.java
@@ -3,9 +3,15 @@ package com.askthem.feed.controller;
 import com.askthem.feed.dto.request.AddQuestion;
 import com.askthem.feed.model.Questions;
 import com.askthem.feed.repository.FeedRepository;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/feed")
@@ -26,7 +32,43 @@ public class FeedController {
     }
 
     @GetMapping("/all")
+//    public ResponseEntity<?> feed(@RequestParam(value="keywords") String keywords, @RequestParam String zipcode){
+    public ResponseEntity<?> feed(@RequestParam("keywords") List<String> values, @RequestParam("zipcode") String zipcode){
+//        List<String> tags = Arrays.asList(keywords.split(","));
+//        List<String> tags = Arrays.asList(values);
+        System.out.println(values);
+        System.out.println(zipcode);
+        String[] val = new String[values.size()];
+        for(int i=0; i<values.size(); ++i){
+            val[i] = values.get(i);
+        }
+
+        System.out.println(val[0]);
+//        return null;
+
+
+
+
+
+        // List
+        List<String> li = new ArrayList<>();
+        li.add("library");
+        li.add("fit");
+
+        Character[] s = { 'f', 'l' };
+
+
+        List<String> tags = List.of("fit", "library");
+//        List<?> tt = new ArrayList<>();
+        String ss = "'fit', 'library'";
+
+        return ResponseEntity.ok().body(feedRepository.listQuestions(ss, zipcode));
+    }
+
+    @GetMapping("/list")
+//    public ResponseEntity<?> feed(@RequestParam(value="keywords") String keywords, @RequestParam String zipcode){
     public ResponseEntity<?> feed(){
-        return ResponseEntity.ok().body(feedRepository.listQuestions());
+        List<String> tags = List.of("Fit", "library");
+        return ResponseEntity.ok().body(feedRepository.listQ(tags));
     }
 }
--- feed/src/main/java/com/askthem/feed/repository/FeedRepository.java
+++ feed/src/main/java/com/askthem/feed/repository/FeedRepository.java
@@ -5,17 +5,24 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Array;
 import java.util.List;
-
 @Repository
 public interface FeedRepository extends MongoRepository<Questions,ObjectId> {
 
     @Aggregation(pipeline = {
-            "{$match: {'tags': {$in:['library','fit']}}, {'zipcode':'575003'} }",
-            "{$sort: {'question':-1}}",
-            "{$project: {'questionId':1, 'question':1, 'zipcode':-1 } }"
+            "{ '$match': {'tags': { '$in': [ ?0 ] } }, { 'zipcode': ?1 } }",
+            "{ '$sort': { 'question':-1 } }",
+            "{ '$project': { 'questionId':1, 'question':1, 'zipcode':-1, 'tags' : 1 } }"
     }, collation = "{ 'locale': 'en', 'strength': 2 }")
-    List<Questions> listQuestions();
+    List<Questions> listQuestions(String s, String zip);
+
+
+    @Query("{ 'tags': { '$in': ?0 } }")
+    List<Questions> listQ(List<String> t);
 }
+
+// ['fit','library']
